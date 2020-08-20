775f29cc6e83fc69a39dab9159ecdd2260b0b53d
miriyala1987 <69935702+miriyala1987@users.noreply.github.com>
2020-08-19 21:36:09 -0500

    Update Hello.java
    old code

--- Hello.java
+++ Hello.java
@@ -1,23 +1,6 @@
-package tester;
-
-public class hello {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- //prints maximum num
-		int a[][]= {{3,2,5},{2,4,6},{5,7,9}};
-		int max=0;
-		for (int i=0;i<3;i++)
-		{
-			for(int j=0;j<3;j++)
-			{
-				if(a[i][j]>max)
-				{
-					max=a[i][j];
-				}
-			}
-		} System.out.println(max);
-		
-	}
-
+public class Hello {
+  public static void main(String[] args) {
+    //prints "Hello World" in the terminal window.
+    System.out.println("Hello World, This is my First Java Project");
+  }
 }
