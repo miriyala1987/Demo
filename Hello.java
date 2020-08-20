063ca3136ac3c28f3a00bccaf244a18c3bde1528
miriyala1987 <69935702+miriyala1987@users.noreply.github.com>
2020-08-19 21:31:05 -0500

    Update Hello.java
    updated script

--- Hello.java
+++ Hello.java
@@ -1,6 +1,23 @@
-public class Hello {
-  public static void main(string[] args) {
-    //prints "Hello World" in the terminal window.
-    system.out.println("Hello World, This is my First Java Project");
-  }
+package tester;
+
+public class multiDimensional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ //prints maximum num
+		int a[][]= {{3,2,5},{2,4,6},{5,7,9}};
+		int max=0;
+		for (int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				if(a[i][j]>max)
+				{
+					max=a[i][j];
+				}
+			}
+		} System.out.println(max);
+		
+	}
+
 }
